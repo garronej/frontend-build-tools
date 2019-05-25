fe08c3cb3acaef073bbecd51a319b2324e45526e
garronej <joseph.garrone.gj@gmail.com>
2019-05-25 19:10:31 +0200

    initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export declare const module_dir_path: string;
+export declare function tsc(tsconfig_path: string, watch?: undefined | "WATCH"): Promise<number | undefined>;
+/** If lessify is required it must be in the page dev-dependencies.*/
+export declare function browserify(entry_point_file_path: string, dst_file_path: string, watch?: undefined | "WATCH"): Promise<number | undefined>;
+export declare function tsc_browserify_minify(tsconfig_path: string, entry_point_file_path: string, out_file_path: string, watch?: undefined | "WATCH"): Promise<void>;
